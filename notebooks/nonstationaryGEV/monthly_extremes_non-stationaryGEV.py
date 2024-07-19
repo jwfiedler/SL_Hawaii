12f82ead34ef164aa948d75e8aedc6678c72fead
jwfiedler <jfiedler@ucsd.edu>
2024-07-18 15:25:24 -1000

    adding annual timescale plot

--- notebooks/nonstationaryGEV/monthly_extremes_non-stationaryGEV.py
+++ notebooks/nonstationaryGEV/monthly_extremes_non-stationaryGEV.py
@@ -91,8 +91,9 @@ def fitness(x, modelType='GEV_SeasonalMu'):
         xmax[cont:cont+2] = [aux[5, 1], aux[6, 1]]
         cont += 2
 
-    if modelType == 'GEV_S_T_Cv':
+    if modelType == 'GEV_S_T_Cv' or modelType == 'GEV_S_T_Cv_Nodal':
         if x[2] == 1:  # Frequency 4w
+            cont -= 2
             xmin[cont:cont+2] = [aux[7, 0], aux[8, 0]]  # should use indices 5 and 6 (same as Semiannual cycle)
             xmax[cont:cont+2] = [aux[7, 1], aux[8, 1]]
             cont += 2
@@ -110,6 +111,12 @@ def fitness(x, modelType='GEV_SeasonalMu'):
         if x[5] == 1:
             xmin[cont] = aux[11, 0]
             xmax[cont] = aux[11, 1]
+            cont += 1
+    
+    if modelType == 'GEV_S_T_Cv_Nodal':
+        if x[6] == 1:
+            xmin[cont:cont+2] = [aux[12, 0], aux[13, 0]]
+            xmax[cont:cont+2] = [aux[12, 1], aux[13, 1]]
             cont += 1
 
 
@@ -242,6 +249,75 @@ def plottingExtremeSeasonality(Jd, T0, Y, w, mio):
 
     plt.show()
 
+def plottingTimeDependentReturnValue(Jd,T0,serieCV0,w):
+    T=T0
+    serieCV=serieCV0
+    dx=0.02
+    t2=np.arange(0,T[-1],dx)
+    
+    years = np.arange(np.floor(T[0]), np.ceil(T[-1])+1)
+
+    psi = w[1]
+    xi = w[2]
+
+
+   
+    x0=w[0] #initial value of return value in the iteraction
+    R=50
+    YR=np.zeros(len(years)-1)
+
+    def Quantilentime(x):
+
+        # Variables previously defined as global in MATLAB, handled here locally
+        b0, b1, b2, b3, b4, b5, b6 = w[0], w[3], w[4], w[5], w[6], w[7], w[8]
+        a0 = w[1]
+        km = 12
+        dt = 0.001
+        ti = np.arange(t00, t11, dt)
+
+        bLT=w[9]
+        bCI=w[10]
+        aCI=w[11]
+        if len(w)<13:
+            bN1=0
+            bN2=0
+        else:
+            bN1=w[12]
+            bN2=w[13]
+
+        # Interpolate serieCV at ti points
+        serieCV2 = np.interp(ti, T, serieCV)
+        # Replace NaNs with zero (np.interp does this by default if outside the bounds)
+        serieCV2[np.isnan(serieCV2)] = 0
+
+        # Define mut and other parameters
+        mut = (b0 * np.exp(bLT * ti) +
+               b1 * np.cos(2 * np.pi * ti) + b2 * np.sin(2 * np.pi * ti) +
+               b3 * np.cos(4 * np.pi * ti) + b4 * np.sin(4 * np.pi * ti) +
+               b5 * np.cos(8 * np.pi * ti) + b6 * np.sin(8 * np.pi * ti) +
+               bN1 * np.cos((2 * np.pi / 18.61) * ti) + bN2 * np.sin((2 * np.pi / 18.61) * ti) +
+               (bCI * serieCV2))
+        psi = a0 + (aCI * serieCV2)
+        xit = xi
+
+        # Calculate factor
+        factor = 0
+        for i in range(len(ti)):
+            h = np.maximum(1 + (xit * (x - mut[i]) / psi[i]), 0.0001) ** (-1 / xit)
+            factor += h
+
+        Pr = 1 - (1 / R)
+
+        y = -Pr + np.exp(-km * factor * dt)
+        return y
+
+    for i in range(len(years)-1):
+        t00=years[i]
+        t11=years[i+1]
+        YR[i]=brentq(Quantilentime,x0-2,x0+2)
+        x0=YR[i]
+    return years, YR
+
 #%%
 # Load preprocessed monthly max data in (date, sea level (m,MHHW)) format
 # read in 'MM_TWL_Kahului_Sigma.mat' file
@@ -367,5 +443,87 @@ SignifCvte2 = chi2.cdf(2 * diffe, p)
 
 print(f'Statistical Significance of Covariate in scale param.: {SignifCvte2*100:.2f}%')
 
+
+# %%
+# Ploting outcomes of the time-dependent extreme model
+
+
+
+
+
+# % --------------------------------
+# % chromosome: [1/0 1/0 1/0 1/0 1/0 1/0 1/0]
+# % 1st gene: annual cycle location p.
+# % 2nd gene: semi-annual cycle location p.
+# % 3rd gene: tri-annual cycle location p.
+# % 4th gene: Trend, location p.
+# % 5th gene: Covariate, location p.
+# % 6th gene: Covariate, scale p.
+# % 7th gene: Nodal cycle
+# % Max number of parameters: 14
+# % =========================================
+# % =========================================
+# disp('Checking Nodal cycle in Location parameter ');
+
+x_N=np.append(x_s[-1],[1, 0, 0, 1]) # Nodal
+
+f_N =fitness(x_N,modelType='GEV_S_T_Cv_Nodal');
+
+w_N = np.loadtxt('best.txt');
+mio = np.loadtxt('mio.txt');
+
+np.savez('Result_Nodal.npz', w_N=w_N, x_N=x_N, mio=mio)
+
+diffe = w_N[0] - w_T[0]
+p = 2
+SignifTrend = chi2.cdf(2 * diffe, p)
+
+print(f'Statistical Significance of Nodal cycle: {SignifTrend*100:.2f}%')
+
+
+# Ploting nodal cycle in location GEV parameter
+# caution! I have modify the best_smu vector for this test case!!
+
+
+
+
+# %%
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+sss = fig.add_subplot(1, 1, 1)
+sss.plot(t+year0, Y, '*k', markersize=4, label='Monthly Maxima')
+
+# Ploting model with only time-dependency within the year
+# best_param is everything except the first element of w_s, and then three zeros
+best_param = np.append(w_s[1:], [0, 0, 0])
+
+years, Yr_s = plottingTimeDependentReturnValue(Jd, t, Cvte, best_param)
+sss.plot(years[:-1]+0.5+year0, Yr_s, 'k', linewidth=2, label='S')
+
+# Ploting model with Seasonality & Trend
+best_param = np.append(w_T[1:], [0, 0])
+years, Yr = plottingTimeDependentReturnValue(Jd, t, Cvte, best_param)
+sss.plot(years[:-1]+0.5+year0, Yr, 'b', linewidth=2, label='S,T')
+
+# Ploting sigma influence in location
+best_smu = np.append(w_cvte1[1:], [0])
+years, Yr_sigMu = plottingTimeDependentReturnValue(Jd, t, Cvte, best_smu)
+sss.plot(years[:-1]+0.5+year0, Yr_sigMu, 'g', linewidth=1, label='Covariate_location')
+
+# Ploting sigma influence in scale
+best_spsi = np.append(w_cvte2[1:-1], [0, w_cvte2[-1]])
+years, Yr_sigPsi = plottingTimeDependentReturnValue(Jd, t, Cvte, best_spsi)
+sss.plot(years[:-1]+0.5+year0, Yr_sigPsi, 'c', linewidth=1, label='Covariate_scale')
+
+best_MuN = np.append(w_N[1:11], [0, 0, w_N[11], w_N[12]])
+[years,Yr_muN]=plottingTimeDependentReturnValue(Jd,t,Cvte,best_MuN); 
+sss.plot(years[:-1]+0.5+year0,Yr_muN,'m',linewidth=1, label = 'Nodal')
+
+sss.set_ylabel('Sea level (m)')
+# add grid
+sss.grid(True)
+# add legend outside the plot
+sss.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
 # %%
